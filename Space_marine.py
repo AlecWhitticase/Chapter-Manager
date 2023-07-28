ed554c9a3b86b9770592561b7b5f5ff24308c21c
AlecWhitticase <awh62@uclive.ac.nz>
2023-07-28 22:01:43 +1200

    Maybe Imma multithread this bad boy

--- Space_marine.py
+++ Space_marine.py
@@ -1,8 +1,8 @@
 import random
 import namelists
-
-
-
+from tkinter import *
+from tkinter.ttk import * 
+import threading
 
 class Location():
     def __init__(self, name):
@@ -21,137 +21,6 @@ for location_name in namelists.locations_name:
     
     # Append the Location object to the locations list
     locations.append(location)
-
-ultramarine_names = [
-    "Marneus Calgar",
-    "Cato Sicarius",
-    "Uriel Ventris",
-    "Aeonid Thiel",
-    "Agemman",
-    "Tigurius",
-    "Aethon",
-    "Avitus",
-    "Tiberius",
-    "Galenus",
-    "Octavian",
-    "Severus",
-    "Cassian",
-    "Valerian",
-    "Corvin",
-    "Decimus",
-    "Lucian",
-    "Lucius",
-    "Maximus",
-    "Gaius",
-    "Marius",
-    "Titus",
-    "Felix",
-    "Lucullus",
-    "Decius",
-    "Pertinax",
-    "Valentinian",
-    "Gaius Julius",
-    "Theodoric",
-    "Aurelius",
-    "Quintus",
-    "Rufus",
-    "Castor",
-    "Cincinnatus",
-    "Didius",
-    "Fabian",
-    "Gracchus",
-    "Hadrian",
-    "Horatius",
-    "Julius",
-    "Justinian",
-    "Marcellus",
-    "Marcus",
-    "Nero",
-    "Ovidius",
-    "Publius",
-    "Remus",
-    "Scipio",
-    "Seneca",
-    "Sulla",
-    "Tacticus",
-    "Valerius",
-    "Vespasian",
-    "Virgil",
-    "Xerxes",
-    "Zephyrus",
-    "Aelian",
-    "Alaric",
-    "Antares",
-    "Aquilon",
-    "Archimedes",
-    "Arcturus",
-    "Ares",
-    "Argus",
-    "Asmodeus",
-    "Aurum",
-    "Bellerophon",
-    "Calibos",
-    "Castellan",
-    "Centurion",
-    "Chromius",
-    "Corvus",
-    "Crassus",
-    "Decimus Maximus",
-    "Dominus",
-    "Draco",
-    "Drusus",
-    "Eleutherios",
-    "Emilian",
-    "Equinox",
-    "Fabius",
-    "Falkus",
-    "Ferox",
-    "Gallus",
-    "Garro",
-    "Gideon",
-    "Gorgon",
-    "Gravitas",
-    "Hector",
-    "Helios",
-    "Ignatius",
-    "Inquisitorius",
-    "Janus",
-    "Javelin",
-    "Jupiter",
-    "Justus",
-    "Kestros",
-    "Korax",
-    "Leonidas",
-    "Libertas",
-    "Lysander",
-    "Magnus",
-    "Marcellian",
-    "Maximilian",
-    "Mortifer",
-    "Nemesis",
-    "Nemean",
-    "Nocturnus",
-    "Octavianus",
-    "Orpheus",
-    "Paladin",
-    "Patricius",
-    "Pentheus",
-    "Phoebus",
-    "Placidus",
-    "Prometheus",
-    "Proteus",
-    "Pyrrhus",
-    "Quirinus",
-    "Radamanthys",
-    "Ragnar",
-    "Remulus",
-    "Romulus",
-    "Sanguinius",
-    "Scarius",
-    "Scipionis",
-    "Septimus",
-    "Seraphim",
-    "Silvanus"]
 
 class Day:
     def __init__(self,number):
@@ -219,15 +88,15 @@ def generate_tenth():
     for squad in tenth_company:
         for i in range(0, 10):
             if i == 0:
-                squad.append(SpaceMarine(random.choice(ultramarine_names),"Scout Sergent"))
+                squad.append(SpaceMarine(random.choice(namelists.ultramarine_names),"Scout Sergent"))
             else:
-                squad.append(SpaceMarine(random.choice(ultramarine_names),"Scout"))
+                squad.append(SpaceMarine(random.choice(namelists.ultramarine_names),"Scout"))
 
     for i in range(0,3):
         if i == 0:
-            command.append(SpaceMarine(random.choice(ultramarine_names),"Captain"))
+            command.append(SpaceMarine(random.choice(namelists.ultramarine_names),"Captain"))
         else:
-            command.append(SpaceMarine(random.choice(ultramarine_names),"Lieutenant"))
+            command.append(SpaceMarine(random.choice(namelists.ultramarine_names),"Lieutenant"))
 
     tenth_company.insert(0,command)
     
@@ -242,30 +111,30 @@ def generate_battle_company():
         if troop_role == 0:
             for i in range(0,3):
                 if i == 0:
-                    squad.append(SpaceMarine(random.choice(ultramarine_names),"Captain"))
+                    squad.append(SpaceMarine(random.choice(namelists.ultramarine_names),"Captain"))
                 else:
-                    squad.append(SpaceMarine(random.choice(ultramarine_names),"Lieutenant"))
+                    squad.append(SpaceMarine(random.choice(namelists.ultramarine_names),"Lieutenant"))
             troop_role += 1
         elif troop_role in [1,2,3,4,5]:
             for i in range(0, 10):
                 if i == 0:
-                    squad.append(SpaceMarine(random.choice(ultramarine_names),"Tactical Sergent"))
+                    squad.append(SpaceMarine(random.choice(namelists.ultramarine_names),"Tactical Sergent"))
                 else:
-                    squad.append(SpaceMarine(random.choice(ultramarine_names),"Tactical Marine"))
+                    squad.append(SpaceMarine(random.choice(namelists.ultramarine_names),"Tactical Marine"))
             troop_role += 1
         elif troop_role in [6,7]:
             for i in range(0, 10):
                 if i == 0:
-                    squad.append(SpaceMarine(random.choice(ultramarine_names),"Assault Sergent"))
+                    squad.append(SpaceMarine(random.choice(namelists.ultramarine_names),"Assault Sergent"))
                 else:
-                    squad.append(SpaceMarine(random.choice(ultramarine_names),"Assault Marine"))
+                    squad.append(SpaceMarine(random.choice(namelists.ultramarine_names),"Assault Marine"))
             troop_role += 1
         else:
             for i in range(0, 10):
                 if i == 0:
-                    squad.append(SpaceMarine(random.choice(ultramarine_names),"Devestator Sergent"))
+                    squad.append(SpaceMarine(random.choice(namelists.ultramarine_names),"Devestator Sergent"))
                 else:
-                    squad.append(SpaceMarine(random.choice(ultramarine_names),"Devestator Marine"))
+                    squad.append(SpaceMarine(random.choice(namelists.ultramarine_names),"Devestator Marine"))
             troop_role += 1
 
     
@@ -280,16 +149,57 @@ def print_company(company):
             print(marine)
         print()
 
+def ui():
+    window = Tk()
+
+    data = StringVar()
+
+    data.set()
+
+    label = Label(window, textvariable=data)
+
+    label.grid(row=0, column=0)
+
+    entry = Entry(window, textvariable=data)
+
+    entry.grid(row=1, column=0)
+
+
+
+    def clear_data(data):
+
+        data.set("")
+
+
+
+    "ffgg"
+
+    clear = Button(window, text="Clear", command=lambda: clear_data(data))
+
+    clear.grid(row=2, column=0)
+
+    quit = Button(window, text="Quit", command=window.destroy)
+
+    quit.grid(row=3, column=0)
+
+    "ggg"
+    window.mainloop() 
+
+
+
 def main():
     tenth_company = Company(generate_tenth(),(0,0))
     second_company = Company(generate_battle_company(),(0,0))
     chapter = [second_company,tenth_company]
     current_day = 1
+    ui_thread = threading.Thread(target=ui)
+    ui_thread.start()
+
     while current_day <= 10:
         new_day = Day(current_day)
         missions = new_day.missions
-        print(missions)
-        day+=1
+        data.set(missions)
+        current_day += 1  
 
 
 main()
